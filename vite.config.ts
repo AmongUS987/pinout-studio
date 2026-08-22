267adff6708a790fd313478f94a5a26b0baabb9b
AmongUS987 <165092320+AmongUS987@users.noreply.github.com>
2026-08-22 20:59:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/pinout-studio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
