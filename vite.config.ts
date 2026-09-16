e7984506cedd2f845a05173f594eefd225ec0c87
AmongUS987 <165092320+AmongUS987@users.noreply.github.com>
2026-09-16 19:32:17 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/pinout-studio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
